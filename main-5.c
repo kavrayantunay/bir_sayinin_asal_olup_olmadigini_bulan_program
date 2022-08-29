dfa0ebc10937728f8235bcb37e63847206ab3107
kavrayantunay <70447638+kavrayantunay@users.noreply.github.com>
2022-08-29 22:06:03 +0300

    Add files via upload

---
+++ main-5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a=1,b;
+    scanf("%d",&b);
+    int flag=0;
+    while(a<b-1){
+        a++;
+        if(b%a==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+        printf("sayi asaldir");
+    else
+        printf("sayi asal değildir");
+    
+}
